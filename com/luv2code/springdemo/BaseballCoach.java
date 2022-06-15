eb30afe1b7c96d534b11ed52f74592a91d0596b0
Samira Marcela Roman <samira1980dev@gmail.com>
2022-06-15 17:57:12 -0300

    ops, esqueci desses

--- com/luv2code/springdemo/BaseballCoach.java
+++ com/luv2code/springdemo/BaseballCoach.java
@@ -2,6 +2,12 @@ package com.luv2code.springdemo;
 
 public class BaseballCoach implements Coach {
 	
+	private FortuneService fortuneService;
+	
+	public BaseballCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	};
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Spend 30 minutes on batting practice";
@@ -9,8 +15,7 @@ public class BaseballCoach implements Coach {
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		return fortuneService.getFortune();
 	}
 
 }
